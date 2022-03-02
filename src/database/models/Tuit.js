d6bb0ea13aacbad6faa9f189aa6b74440cc21cf5
Josep Conejero <bermeu@gmail.com>
2022-03-02 18:59:57 +0100

    Added Tuit.js

---
+++ src/database/models/Tuit.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require("mongoose");
+
+const TuitSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+    timestamps: true,
+  },
+  text: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 200,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Tuit = model("Tuit", TuitSchema, "tuits");
+
+module.exports = Tuit;
