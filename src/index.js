c1a6c3f6366ea4d32b923fa69694696f76746982
Josep Conejero <bermeu@gmail.com>
2022-03-02 18:19:45 +0100

    Added ./src/index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const debug = require("debug")("tuitah:root");
+const app = require("./server/index");
+const connectToDataBase = require("./database/index");
+const riseTheServer = require("./riseTheServer");
+
+const port = process.env.PORT || 4000;
+const mongoConnection = process.env.MONGO_STRING_PRODUCTION;
+
+(async () => {
+  try {
+    await connectToDataBase(mongoConnection);
+
+    await riseTheServer(port, app);
+  } catch (error) {
+    debug(`Error: ${error.message}`);
+  }
+})();
